e627fbac7282f1d427e46446e90684c090c9facc
Regan Dee <reganjduthie@gmail.com>
2017-11-16 17:51:21 +0200

    Portfolio - Three.js Added

--- js/graphics.js
+++ js/graphics.js
@@ -1,5 +1,6 @@
 var scene1 = new THREE.Scene();
 var scene2 = new THREE.Scene();
+var scene3 = new THREE.Scene();
 
 var renderer1 = new THREE.WebGLRenderer({
     antialias: true,
@@ -12,12 +13,17 @@ var renderer2 = new THREE.WebGLRenderer({
     alpha: true
 });
 
+var renderer3 = new THREE.WebGLRenderer({
+    antialias: true,
+    alpha: true
+});
+
+//Canvas'
 var canvas1 = document.getElementById("three-container");
 var canvas2 = document.getElementById("three-container_skills");
+var canvas3 = document.getElementById("threejs_portfolio");
 
-var camera1 = new THREE.PerspectiveCamera(45, CANVAS1_WIDTH / CANVAS1_HEIGHT, 0.1, 100);
 
-var camera2 = new THREE.PerspectiveCamera(45, CANVAS2_WIDTH / CANVAS2_HEIGHT, 0.1, 100);
 
 var CANVAS1_WIDTH = 500;
 var CANVAS1_HEIGHT = 500;
@@ -25,6 +31,15 @@ var CANVAS1_HEIGHT = 500;
 
 var CANVAS2_WIDTH = 500;
 var CANVAS2_HEIGHT = 500;
+
+var CANVAS3_WIDTH = 500;
+var CANVAS3_HEIGHT = 500;
+
+var camera1 = new THREE.PerspectiveCamera(45, CANVAS1_WIDTH / CANVAS1_HEIGHT, 0.1, 100);
+
+var camera2 = new THREE.PerspectiveCamera(45, CANVAS2_WIDTH / CANVAS2_HEIGHT, 0.1, 100);
+
+var camera3 = new THREE.PerspectiveCamera(45, CANVAS3_WIDTH / CANVAS3_HEIGHT, 0.1, 100);
 
 var mesh1 = null;
 var meAvatar;
@@ -195,6 +210,9 @@ function setCanvasSize() {
 
     CANVAS2_WIDTH = document.getElementById('three-container_skills').offsetWidth;
     CANVAS2_HEIGHT = document.getElementById('three-container_skills').offsetHeight;
+    
+    CANVAS3_WIDTH = document.getElementById('threejs_portfolio').offsetWidth;
+    CANVAS3_HEIGHT = document.getElementById('threejs_portfolio').offsetHeight;
 
 
 };
@@ -209,6 +227,11 @@ camera1.lookAt(scene1.position);
 camera2.position.x = 0;
 camera2.position.y = 4.5;
 camera2.position.z = 15;
+
+camera3.position.x = 0;
+camera3.position.y = 0;
+camera3.position.z = 0;
+
 //camera2.lookAt(meAvatar.position);
 
 
@@ -238,6 +261,8 @@ scene2.add(hemLight);
 canvas1.appendChild(renderer1.domElement);
 
 canvas2.appendChild(renderer2.domElement);
+
+canvas3.appendChild(renderer3.domElement);
 
 
 loadMesh();
@@ -275,4 +300,14 @@ function render() {
 
     camera2.aspect = CANVAS2_WIDTH / CANVAS2_HEIGHT;
     camera2.updateProjectionMatrix();
+    
+    //Renderer-3
+
+    renderer3.autoClear = false;
+    renderer3.clear();
+    renderer3.render(scene2, camera2);
+    renderer3.setSize(CANVAS2_WIDTH, CANVAS2_HEIGHT);
+
+    camera3.aspect = CANVAS2_WIDTH / CANVAS2_HEIGHT;
+    camera3.updateProjectionMatrix();
 };
